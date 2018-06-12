dd79555a6d339a22a05e4f11dc3ecb575bef4f8f
Huấn Phan Minh <pmhuan@tma.com.vn>
2018-06-12 16:00:02 +0700

    Test GitHub Desktop

---
+++ src/main/java/com/training/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.training.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.training.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+}
